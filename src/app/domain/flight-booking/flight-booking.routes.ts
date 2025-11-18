50fde6ea7f981a34c40a481a6d2ce07b8de05165
Bruno Donatelli <bruno.donatelli17@gmail.com>
2025-11-18 17:34:54 -0300

    feat(flight-booking): create component

---
+++ src/app/domain/flight-booking/flight-booking.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from "@angular/router";
+import { FlightBooking } from "./flight-booking";
+
+export const FLIGHT_BOOKING_ROUTES: Routes = [
+  { path: '', component: FlightBooking }
+];
---
+++ src/app/domain/flight-booking/flight-booking.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-flight-booking',
+  imports: [],
+  templateUrl: './flight-booking.html',
+  styleUrl: './flight-booking.scss',
+})
+export class FlightBooking {
+
+}
