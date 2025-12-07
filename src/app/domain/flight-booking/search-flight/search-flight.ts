1179339c72da923cb218b36d149b7eff9cc58c2c
Bruno Donatelli <bruno.donatelli17@gmail.com>
2025-12-07 13:47:46 -0300

    feat(booking-signals): add titles

--- src/app/domain/flight-booking/search-flight/search-flight.ts
+++ src/app/domain/flight-booking/search-flight/search-flight.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ProgressStep } from '../../../shared/components/progress-step/progress-step';
+import { Viacep } from '../../../shared/services/viacep';
 
 @Component({
   selector: 'app-search-flight',
-  imports: [],
+  imports: [ProgressStep],
   templateUrl: './search-flight.html',
   styleUrl: './search-flight.scss',
 })
 export class SearchFlight {
+  private readonly viaCepService = inject(Viacep);
 
+  currentStep = 0;
+
+  steps = [
+    'Flight Details',
+    'Passengers',
+    'Services',
+    'Payment'
+  ];
 }
